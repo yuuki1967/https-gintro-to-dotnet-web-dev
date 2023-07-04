1df832cb11692a66b8ca4941474133e584289580
Yuki Endo <yuuki1967@github.com>
2023-07-05 08:40:05 +0900

    Update Privacy.cshtml.cs
    [skip ci]

--- 3-razor-pages/0-start/RazorPagesPizza/RazorPagesPizza/Pages/Privacy.cshtml.cs
+++ 3-razor-pages/0-start/RazorPagesPizza/RazorPagesPizza/Pages/Privacy.cshtml.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Data.SQLite;
 
 namespace RazorPagesPizza.Pages
 {
@@ -15,34 +12,8 @@ namespace RazorPagesPizza.Pages
             _logger = logger;
         }
 
-        using Microsoft.AspNetCore.Mvc;
-        using Microsoft.AspNetCore.Mvc.RazorPages;
-        using Microsoft.Extensions.Logging;
-        using System;
-        using System.Data.SQLite;
-
-        namespace RazorPagesPizza.Pages
+        public void OnGet()
         {
-            public class PrivacyModel : PageModel
-            {
-                private readonly ILogger<PrivacyModel> _logger;
-
-                public PrivacyModel(ILogger<PrivacyModel> logger)
-                {
-                    _logger = logger;
-                }
-
-                public void OnPost()
-                {
-                    SQLiteConnectionStringBuilder builder = new SQLiteConnectionStringBuilder();
-                    builder.DataSource = "mydb.db";
-                    builder.Version = 3;
-                    string connectionString = builder.ConnectionString;
-                    // use the connection string to connect to the database
-                }
-            }
         }
-        public void OnPost()
-        {}
     }
 }
